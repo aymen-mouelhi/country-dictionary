8d814959cae1af5b4a467c4618ee6088adf0ae3e
Aymen Mouelhi <aymen.mouelhi@gmail.com>
2016-04-14 00:52:10 +0200

    Sparql integration

--- index.js
+++ index.js
@@ -138,6 +138,16 @@ CountryDictionary.prototype.getCities = function(country, callback) {
 };
 
 /**
+ * Get Cities using SPARQL
+ * @param  {[type]}   country  [description]
+ * @param  {Function} callback [description]
+ * @return {[type]}            [description]
+ */
+CountryDictionary.prototype._getCities - function(country, callback){
+
+}
+
+/**
  * Get Country By Name
  * @param  {String} country Country's Name
  * @return {[type]}         [description]
